4930c0e104ac299171b83d576c325101a60d5f72
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2019-09-11 23:04:51 -0300

    Update gravar_bd.py

--- gravar_bd.py
+++ gravar_bd.py
@@ -1,23 +1,45 @@
 from kafka import KafkaConsumer
+import requests
+import json, os
 
-# To consume latest messages and auto-commit offsets
-broker = "kafka.kubeless:9092"
-consumer = KafkaConsumer('urls', bootstrap_servers=[broker])
-for message in consumer:
-    # message value and key are raw bytes -- decode if necessary!
-    # e.g., for unicode: `message.value.decode('utf-8')`
-    print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
-                                          message.offset, message.key,
-                                          message.value))
+'''
+Modelo: 
+URLS = {
+    "link1": {
+        "shorturl": "link1",
+        "link": "http://tonanuvem.net",
+    },
+    "link2": {
+        "shorturl": "link2",
+        "link": "http://slack.com",
+    },
+}
 
-# consume earliest available messages, don't commit offsets
-#KafkaConsumer(auto_offset_reset='earliest', enable_auto_commit=False)
+def get_timestamp():
+    return datetime.now().strftime(("%Y-%m-%d %H:%M:%S"))
+'''
 
-# consume json messages
-#KafkaConsumer(value_deserializer=lambda m: json.loads(m.decode('ascii')))
+def bd(event, context):
+    print(event)
 
-# consume msgpack
-#KafkaConsumer(value_deserializer=msgpack.unpackb)
+    # To consume latest messages and auto-commit offsets
+    broker = "kafka.kubeless:9092"
+    consumer = KafkaConsumer('urls', bootstrap_servers=[broker])
+    for message in consumer:
+        # message value and key are raw bytes -- decode if necessary!
+        # e.g., for unicode: `message.value.decode('utf-8')`
+        print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
+                                              message.offset, message.key,
+                                              message.value))
 
-# StopIteration if no message after 1sec
-KafkaConsumer(consumer_timeout_ms=1000)
+    # consume earliest available messages, don't commit offsets
+    #KafkaConsumer(auto_offset_reset='earliest', enable_auto_commit=False)
+
+    # consume json messages
+    #KafkaConsumer(value_deserializer=lambda m: json.loads(m.decode('ascii')))
+
+    # consume msgpack
+    #KafkaConsumer(value_deserializer=msgpack.unpackb)
+
+    # StopIteration if no message after 1sec
+    KafkaConsumer(consumer_timeout_ms=1000)

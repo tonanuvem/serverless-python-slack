0802d8255d4befbf3b5dfa29abc0967ccc4ff0b7
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2019-09-13 15:17:37 -0300

    Update slack.py

--- slack.py
+++ slack.py
@@ -5,23 +5,29 @@ url = "https://hooks.slack.com/services/TH8SKHYGZ/BHF7V6PJ4/VRrDsfK5fZuWJ6xNoANB
 canal = "#lab-testes"
 
 def handler(event, context):
-    print(event)
-    if not 'msg' in event['data']:
-        return 'Campo vazio : msg'
-    #exemplo de uso: kubeless function call slack --data '{"msg": "ALUNO: Bem vindo ao mundo do Serverless"}'
-    #obs: ao rodar via command line, não há suporte caracteres especiais (ex: assentos)
-    texto=event['data']['msg']
-    print("Texto = "+ texto)
-    return postMSG_criada_para_o_slack(texto)
+    try:
+        print(event)
+        if not 'msg' in event['data']:
+            return 'Campo vazio : msg'
+        #exemplo de uso: kubeless function call slack --data '{"msg": "ALUNO: Bem vindo ao mundo do Serverless"}'
+        #obs: ao rodar via command line, não há suporte caracteres especiais (ex: assentos)
+        texto=event['data']['msg']
+        print("Texto = "+ texto)
+        return postMSG_criada_para_o_slack(texto)
+        
+    except Exception as e:
+        erro = "Erro na function: " + repr(e);
+        print(erro)
+        return erro
     
 def postMSG_criada_para_o_slack(msg):
     # format payload for slack
     sdata = formatForSlack(msg)
     r = requests.post(url, sdata, headers={'Content-Type': 'application/json'})
     if r.status_code == 200:
-      print('SUCCEDED: Sent slack webhook')
+      return 'SUCCEDED: Sent slack webhook.  Msg = ' + msg
     else:
-      print('FAILED: Send slack webhook')
+      return 'FAILED: Send slack webhook'
 
 def formatForSlack(msg):
   payload = {
